fa111fc1c1c3869c0e1be5d188adc34d31a0942f
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-04-17 15:15:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
- var a, b
-
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
 
 function add() {
   return a += b
